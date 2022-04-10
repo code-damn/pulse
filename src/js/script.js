8e32e1b9d64568e1a2bf821b6d470cc8380e6fc1
Roman <maryushkinroman91@yandex.ru>
2022-04-10 12:36:03 +0300

    add_map

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
+	$('section.carousel:not(.carousel__inner)').css("display", "block");
+
 	$('.carousel__inner').slick({
-		dots: true,
+		dots: false,
+		dotsClass: 'slick-dots',
 		infinite: true,
 		speed: 900,
 		slidesToShow: 1,
-		adaptiveHeight: false,
 		autoplay: false,
 		autoplaySpeed: 900,
-		boolean: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
 		responsive: [
